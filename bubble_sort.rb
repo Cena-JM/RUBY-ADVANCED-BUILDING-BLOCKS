cea212117ee793f1cdad24e35f044cde7345be4d
Zeha Irawan <jangkaringbumi@gmail.com>
2019-03-20 20:50:19 +0700

    remove some spaces

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,11 +1,11 @@
 def bubble_sort(a)
   (0...a.length - 1).each do |n|
-    (0...a.length - 1-n).each do |m|
-      if a[m] > a[m+1]
-          a[m], a[m+1] = a[m+1], a[m]
+    (0...a.length - 1 - n).each do |m|
+      if a[m] > a[m + 1]
+        a[m], a[m + 1] = a[m + 1], a[m]
       end
     end
   end
-print a
+  print a
 end
 bubble_sort([4, 3, 78, 2, 0, 5])
