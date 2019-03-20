c19e1f75307945de029a9586b0b651f523421848
Zeha Irawan <jangkaringbumi@gmail.com>
2019-03-20 22:26:04 +0700

    separting the bubble sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -7,15 +7,4 @@ def bubble_sort(arr)
   print arr
 end
 
-def bubble_sort_by(word)
-  (0...word.length - 1).each do |n|
-    (0...word.length - 1 - n).each do |m|
-      word[m], word[m + 1] = word[m + 1], word[m] if word[m] < word[m + 1]
-    end
-  end
-  print word
-end
-
-bubble_sort_by(%w[hi hello hey])
-
 bubble_sort([4, 3, 78, 2, 0, 5])
