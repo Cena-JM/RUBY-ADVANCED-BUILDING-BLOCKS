5ebf5ce1312d197ce4765daa084bcd0b04175bba
Zeha Irawan <jangkaringbumi@gmail.com>
2019-03-20 21:51:39 +0700

    basic bubble sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -6,4 +6,16 @@ def bubble_sort(arr)
   end
   print arr
 end
+
+def bubble_sort_by(arr)
+  (0...arr.length - 1).each do |n|
+    (0...arr.length - 1 - n).each do |m|
+      arr[m], arr[m + 1] = arr[m + 1], arr[m] if arr[m] < arr[m + 1]
+    end
+  end
+  print arr
+end
+
+bubble_sort_by(%w[hi hello hey])
+
 bubble_sort([4, 3, 78, 2, 0, 5])
