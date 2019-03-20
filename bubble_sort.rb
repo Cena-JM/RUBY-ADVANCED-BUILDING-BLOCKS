b95b7ea4bce4ee698264a532c251839abdc80b59
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-03-20 16:44:43 +0300

    changed indentation

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,9 +3,9 @@ def bubble_sort(a)
     (0...a.length - 1-n).each do |m|
       if a[m] > a[m+1]
           a[m], a[m+1] = a[m+1], a[m]
-     end
-   end
- end
+      end
+    end
+  end
 print a
 end
 bubble_sort([4, 3, 78, 2, 0, 5])
