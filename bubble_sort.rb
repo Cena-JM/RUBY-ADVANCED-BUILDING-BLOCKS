f82fffe7ccb394c3e50ab81ed3f95e83f6ea7fa4
Zeha Irawan <jangkaringbumi@gmail.com>
2019-03-20 21:01:05 +0700

    fix if single line body

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,11 +1,9 @@
-def bubble_sort(a)
-  (0...a.length - 1).each do |n|
-    (0...a.length - 1 - n).each do |m|
-      if a[m] > a[m + 1]
-        a[m], a[m + 1] = a[m + 1], a[m]
-      end
+def bubble_sort(arr)
+  (0...arr.length - 1).each do |n|
+    (0...arr.length - 1 - n).each do |m|
+      arr[m], arr[m + 1] = arr[m + 1], arr[m] if arr[m] > arr[m + 1]
     end
   end
-  print a
+  print arr
 end
 bubble_sort([4, 3, 78, 2, 0, 5])
