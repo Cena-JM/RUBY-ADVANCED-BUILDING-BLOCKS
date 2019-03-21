4c34608aca1a09d12d3d68fd4f082eb956d86aef
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-03-21 13:22:34 +0300

    Removed repeated assignments with a while loop

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,12 +1,11 @@
 def bubble_sort(arr)
-  (0...arr.length - 1).each do |n|
-    (0...arr.length - 1 - n).each do |m|
-      arrm1 = arr[m + 1]
-      arrm = arr[m]
-      arrm, arrm1 = arrm1, arrm if arrm > arrm1
+  n = arr.size
+  while n >= 0
+    (0...n - 1).each do |m|
+      arr[m], arr[m + 1] = arr[m + 1], arr[m] if arr[m] > arr[m + 1]
     end
+    n -= 1
   end
   print arr
 end
-
 bubble_sort([4, 3, 78, 2, 0, 5])
