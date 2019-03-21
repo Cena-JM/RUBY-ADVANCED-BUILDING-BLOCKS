cafdffb18ce3ad7506011cd3fc66e05d47b87c3f
Zeha Irawan <jangkaringbumi@gmail.com>
2019-03-21 18:16:48 +0700

    refactor n-1

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,7 +1,8 @@
 def bubble_sort(arr)
   n = arr.count
+  m = n - 1
   while n >= 0
-    (0...n - 1).each do |m|
+    (0...m).each do |m|
       arr[m], arr[m + 1] = arr[m + 1], arr[m] if arr[m] > arr[m + 1]
     end
     n -= 1
