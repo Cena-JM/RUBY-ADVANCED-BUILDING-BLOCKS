4bde56fae2ab6870058c9fad73048b5facdf4a4a
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-03-21 13:31:58 +0300

    removed un-wanted folders

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,10 +1,1 @@
-def bubble_sort_by(word)
-  (0...word.length - 1).each do |n|
-    (0...word.length - 1 - n).each do |m|
-      word[m], word[m + 1] = word[m + 1], word[m] if word[m] < word[m + 1]
-    end
-  end
-  print word
-end
 
-bubble_sort_by(%w[hi hello hey])
