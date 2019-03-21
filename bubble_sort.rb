b8e28b114c1c39bfb6960bdbe0783c460824f44c
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-03-21 14:43:34 +0300

    removed unwated variable

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,8 +1,7 @@
 def bubble_sort(arr)
   n = arr.count
-  n_minus_one = n - 1
   while n >= 0
-    (0...n_minus_one).each do |m|
+    (0...n - 1).each do |m|
       arr[m], arr[m + 1] = arr[m + 1], arr[m] if arr[m] > arr[m + 1]
     end
     n -= 1
