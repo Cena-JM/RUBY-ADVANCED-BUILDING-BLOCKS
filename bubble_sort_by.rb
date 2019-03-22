cdc2af7e85072db20f3b8c8c46c49f5f551ddfc1
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-03-22 12:28:57 +0300

    Added empty line after guard clause

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -5,6 +5,7 @@ def bubble_sort_by(array)
     (1...array_length).each do |k|
       i = yield(array[k - 1], array[k])
       next unless i > 0
+
       array[k], array[k - 1] = array[k - 1], array[k], swap = true
     end
   end
